d010004a67c94e60dd7258549b461713d36b971b
Zach <zach.c.barnes@gmail.com>
2021-08-21 17:00:45 -0700

    FEAT: Add masonary

---
+++ packages/client/src/components/Masonry/Grid.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps<{
+  columns: number
+  rows: number
+}>()
+</script>
+
+<template>
+  <div :class="`grid grid-rows-${props.rows} grid-cols-${props.columns} gap-4`">
+    <slot></slot>
+  </div>
+</template>
---
+++ packages/client/src/components/Masonry/Item.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+// import Spinner from '~/components/Spinner/Spinner.vue'
+
+// const props = withDefaults(defineProps<{
+//   title: string
+//   loading: boolean
+// }>(), {
+//   title: 'Loading...',
+//   loading: true,
+// })
+</script>
+
+<template>
+  <div class="item bg-dark-500 shadow rounded h-75 skeleton-box">
+    <!-- <h3>{{ props.title }}</h3>
+
+    <Spinner v-show="loading"></Spinner> -->
+  </div>
+</template>
+
+<style>
+
+</style>
