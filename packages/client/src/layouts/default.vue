6c512c1a6d69c166597ac3669b48c85e4ea3f9aa
Zach <zach.c.barnes@gmail.com>
2021-08-21 17:01:08 -0700

    FEAT: Add header to page

--- packages/client/src/layouts/default.vue
+++ packages/client/src/layouts/default.vue
@@ -1,5 +1,10 @@
+<script setup lang="ts">
+// import Header from '~/components/Header/Header.vue'
+</script>
+
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    <Header></Header>
     <router-view />
     <Footer />
     <div class="mt-5 mx-auto text-center opacity-25 text-sm">
