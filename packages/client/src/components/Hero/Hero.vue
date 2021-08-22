1f0f87b2f0a312a7b14ab533f41516a5a7876465
Zach <zach.c.barnes@gmail.com>
2021-08-21 17:00:10 -0700

    FEAT: Add Hero

---
+++ packages/client/src/components/Hero/Hero.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-pattern-topography h-100  rounded-xl">
+    <!-- <p>Hello</p> -->
+  </div>
+</template>
+
+<style>
+
+</style>
